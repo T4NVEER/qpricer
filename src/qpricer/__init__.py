c692979e42a1e75a944c6acd11f3283716618955
Tanveer <tanveerkapoor777@gmail.com>
2026-08-09 21:05:00 +0100

    feat: export analytic and tree pricers from package root

--- src/qpricer/__init__.py
+++ src/qpricer/__init__.py
@@ -1,7 +1,10 @@
 """qpricer: European option pricing by analytic, lattice and Monte Carlo methods."""
 
+from qpricer.analytic.black_scholes import price as bs_price
+from qpricer.analytic.implied_vol import implied_vol
 from qpricer.instruments import EuropeanOption, OptionType
 from qpricer.market import MarketData
+from qpricer.tree.binomial import crr_price
 
 __version__ = "0.0.1"
 
@@ -10,4 +13,7 @@ __all__ = [
     "MarketData",
     "OptionType",
     "__version__",
+    "bs_price",
+    "crr_price",
+    "implied_vol",
 ]
