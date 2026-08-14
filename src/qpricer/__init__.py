749089fe00e34edbe7835ef2d4f91c1f50cadc35
Tanveer <tanveerkapoor777@gmail.com>
2026-08-14 21:15:00 +0100

    feat: export Monte Carlo engine from package root

--- src/qpricer/__init__.py
+++ src/qpricer/__init__.py
@@ -4,16 +4,19 @@ from qpricer.analytic.black_scholes import price as bs_price
 from qpricer.analytic.implied_vol import implied_vol
 from qpricer.instruments import EuropeanOption, OptionType
 from qpricer.market import MarketData
+from qpricer.mc.engine import MCResult, mc_price
 from qpricer.tree.binomial import crr_price
 
 __version__ = "0.0.1"
 
 __all__ = [
     "EuropeanOption",
+    "MCResult",
     "MarketData",
     "OptionType",
     "__version__",
     "bs_price",
     "crr_price",
     "implied_vol",
+    "mc_price",
 ]
