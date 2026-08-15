8abb0605c97c27d64229a2e3dc01c190de26b693
Tanveer <tanveerkapoor777@gmail.com>
2026-08-15 15:45:00 +0100

    feat: export variance reduction estimators from package root

--- src/qpricer/__init__.py
+++ src/qpricer/__init__.py
@@ -5,6 +5,12 @@ from qpricer.analytic.implied_vol import implied_vol
 from qpricer.instruments import EuropeanOption, OptionType
 from qpricer.market import MarketData
 from qpricer.mc.engine import MCResult, mc_price
+from qpricer.mc.variance_reduction import (
+    mc_price_antithetic,
+    mc_price_antithetic_cv,
+    mc_price_control_variate,
+    variance_ratios,
+)
 from qpricer.tree.binomial import crr_price
 
 __version__ = "0.0.1"
@@ -19,4 +25,8 @@ __all__ = [
     "crr_price",
     "implied_vol",
     "mc_price",
+    "mc_price_antithetic",
+    "mc_price_antithetic_cv",
+    "mc_price_control_variate",
+    "variance_ratios",
 ]
